1fb294db182a970cddcc52b02ab4f1649b430c5d
Ticlext-Altihaf <yusufsekhanaltaf@smknegeri1garut.sch.id>
2022-08-29 11:28:39 +0700

    Add theme

--- application/views/v_tampil.php
+++ application/views/v_tampil.php
@@ -1,55 +1,76 @@
 <!DOCTYPE html>
-<html>
+<html lang="id">
 <head>
-    <title>Read</title>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<!-- Bootstrap CSS -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+		  integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+	<title>Read</title>
 </head>
 <body>
 <center><h1>Membuat CRUD (Create, Read, Update, Delete) dengan CodeIgniter 3</h1></center>
 <center>
 	<div>
-		<?php echo anchor('create','Tambah Data (Create)'); ?>
+		<?php echo anchor('create', 'Tambah Data (Create)'); ?>
 		<br>
-		<?php echo anchor('create/automatic','Tambah Data Otomatis (Create)'); ?>
+		<?php echo anchor('create/automatic', 'Tambah Data Otomatis (Create)'); ?>
 		<br>
-		<?php echo anchor('delete-all','Hapus Semua Data (Delete)'); ?>
+		<?php echo anchor('delete-all', 'Hapus Semua Data (Delete)'); ?>
 		<br>
 		<p>Jumlah Data: <?php echo $count; ?></p>
 	</div>
 </center>
-<table style="margin:20px auto;" border="1">
-    <tr>
-        <th>No</th>
-        <th>Nama</th>
-        <th>Alamat</th>
-        <th>Pekerjaan</th>
-        <th>Action</th>
-    </tr>
-    <?php
-    #source: malescoding.com
-    $no = 1;
-    foreach($users as $u){
-        ?>
-        <tr>
-            <td><?php echo $no++ ?></td>
-            <td><?php echo $u['nama'] ?></td>
-            <td><?php echo $u['alamat'] ?></td>
-            <td><?php echo $u['perkerjaan'] ?></td>
-            <td>
-                <?php echo anchor('update/'.$u['id'],'Edit (Update)'); ?>
-                <?php echo anchor('delete/'.$u['id'],'Hapus (Delete)'); ?>
-            </td>
-        </tr>
-    <?php } ?>
+<div class="table-responsive">
+<table class="table table-striped table-bordered">
+	<thead class="thead-dark">
+	<tr>
+		<th scope="col">No</th>
+		<th>Nama</th>
+		<th>Alamat</th>
+		<th>Pekerjaan</th>
+		<th>Action</th>
+	</tr>
+	</thead>
+
+	<?php
+	#source: malescoding.com
+	$no = 1;
+	foreach ($users as $u) {
+		?>
+		<tr>
+			<td><?php echo $no++ ?></td>
+			<td><?php echo $u['nama'] ?></td>
+			<td><?php echo $u['alamat'] ?></td>
+			<td><?php echo $u['perkerjaan'] ?></td>
+			<td>
+				<a class="btn btn-primary" href="<?php echo site_url('update/' . $u['id']); ?>">Ubah Data (Update)</a>
+				<a class="btn btn-danger" href="<?php echo site_url('delete/' . $u['id']); ?>">Hapus Data (Delete)</a>
+			</td>
+		</tr>
+	<?php } ?>
 </table>
-</body>
+</div>
 <footer style="
 background-color: #f5f5f5;
 text-align: center;
 padding: 20px;
 ">
-	<p>Not-Copyright; 2022 - <?php echo date('Y'); ?> <a href="https://github.com/Ticlext-Altihaf/codeigniter-3-crud">github.com/Ticlext-Altihaf/codeigniter-3-crud</a></p>
+	<p>Not-Copyright; 2022 - <?php echo date('Y'); ?> <a href="https://github.com/Ticlext-Altihaf/codeigniter-3-crud">github.com/Ticlext-Altihaf/codeigniter-3-crud</a>
+	</p>
 
 </footer>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
+		integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p"
+		crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
+		integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF"
+		crossorigin="anonymous"></script>
+</body>
+
 </html>
 
 
